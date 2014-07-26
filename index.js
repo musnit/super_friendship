f87f9a79ab03a28fefd44f11664b240bea640a6e
musnit <musnit@gmail.com>
2014-07-26 15:20:53 +0200

    Moved node app to root folder, and fixed ports and Procfile for heroku

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
 
 app.get('/', function(req, res){
