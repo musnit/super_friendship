182d8978d6180a02673d734f7988457dac6c15ea
Tim Lind <tim@incremental.co>
2014-07-26 12:48:06 +0200

    draft of receive / broadcast ping

--- server/index.js
+++ server/index.js
@@ -6,30 +6,47 @@ var io = require('socket.io')(http);
 var user = {};
 
 io.on('connection', function(socket){
-	io.on('register', function(user) {
-		register(user);
-	});
-	io.on('ping', function(ping) {});
-});
 
+	io.on('register', function(user) {
+		register(socket, user);
+	});
+
+	io.on('ping', function(ping) {
+		receivePing(socket, ping);
+	});
+
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
 
-function register(user) {
+function register(socket, user) {
+	console.log("New user", user);
+
 	users[user.nick] = user;
 }
 
+// store then broadcast ping
 function receivePing(socket, ping) {
+	console.log("Received ping", ping);
+
 	var nick = ping.nick;
 
 	if (!(nick in users)) {
 
 	}
+
+	var user = users[nick];
+
+	users.ping = ping;
+
+	broadcastPing(socket, ping);
 }
 
-function broadCastPing(socket, ping) {
+function broadcastPing(socket, ping) {
+	console.log("Broadcasting ping", ping);
 
+	io.emit(socket, 'ping', ping);
 }
